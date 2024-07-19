01cb716f1799d8326bcef98970899bb117bd99fc
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-07-19 18:37:04 +0530

    Random Numbers (block.timestamp)

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -1,4 +1,4 @@
-// Layout of Contract: 
+// Layout of Contract:
 // version ✅
 // imports ✅
 // errors ✅
@@ -25,39 +25,57 @@ pragma solidity ^0.8.19;
 /**
  * @title A Sample Raffle contract
  * @author Shoaib Khan
- * @notice This contract is for creating a Sample Raffle 
+ * @notice This contract is for creating a Sample Raffle
  * @dev Implements Chainlink VRFv2.5
  */
 
 contract Raffle {
-    /** Errors */
+    /**
+     * Errors
+     */
     error Raffle_SendMoreToEnterRaffle();
 
     uint256 private immutable i_entranceFee;
+    // @dev The duration of the lottery in seconds
+    uint256 private immutable i_interval;
     address payable[] private s_players;
+    uint256 private s_lastTimeStamp;
 
-    /** Events */
+    /**
+     * Events
+     */
     event RaffleEntered(address indexed players);
 
-    constructor(uint256 entranceFee) {
+    constructor(uint256 entranceFee, uint256 interval) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        s_lastTimeStamp = block.timestamp;
     }
 
-    function enterRaffle() public payable {
-        // require(msg.value >= i_entranceFee, "Not Enough ETH to Sent");  
-        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());   
-        if(msg.value < i_entranceFee) {       // ✅ This is most gas efficient
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not Enough ETH to Sent");
+        // require(msg.value >= i_entranceFee, SendMoreToEnterRaffle());
+        if (msg.value < i_entranceFee) {
+            // ✅ This is most gas efficient
             revert Raffle_SendMoreToEnterRaffle();
         }
         s_players.push(payable(msg.sender));
         emit RaffleEntered(msg.sender);
     }
 
-    function pickWinner() public {
-        // Pick the winner
+    // 1. Get a randome number
+    // 2. Use a random number to pick a winner
+    // 3. Be automatically called
+    function pickWinner() external view {
+        // Check to see if enough time has passed
+        if ((block.timestamp - s_lastTimeStamp) < i_interval) {
+            revert();
+        }
     }
 
-    /**Getter Functions */
+    /**
+     * Getter Functions
+     */
     function getEntranceFee() external view returns (uint256) {
         return i_entranceFee;
     }
